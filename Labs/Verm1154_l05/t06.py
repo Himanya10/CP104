bac0d77e4570c8fea968e37e087f7af787a61146
Himanya10 <145639852+Himanya10@users.noreply.github.com>
2024-04-27 01:24:50 -0400

    Add files via upload

---
+++ Labs/Verm1154_l05/t06.py
@@ -0,0 +1,19 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  Himanya Verma
+ID:      169051154
+Email:   verm1154@mylaurier.ca
+__updated__ = "2023-10-14"
+-------------------------------------------------------
+"""
+# Imports
+# Constants
+
+from functions import is_divisible
+n = 15
+i = 3
+j = 5
+result = is_divisible(n, i, j)
+print(result)
---
+++ Labs/Verm1154_l05/t09.py
@@ -0,0 +1,18 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  Himanya Verma
+ID:      169051154
+Email:   verm1154@mylaurier.ca
+__updated__ = "2023-10-14"
+-------------------------------------------------------
+"""
+# Imports
+# Constants
+
+from functions import wind_speed
+
+speed = 95
+category = wind_speed(speed)
+print(category)
