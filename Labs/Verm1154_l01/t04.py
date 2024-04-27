08c961cc347fbec8b6e0b1118d85c6dc636e0caf
Himanya10 <145639852+Himanya10@users.noreply.github.com>
2024-04-27 01:21:31 -0400

    Add files via upload

---
+++ Labs/Verm1154_l01/t04.py
@@ -0,0 +1,15 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  Himanya Verma
+ID:      169051154
+Email:   verm1154@mylaurier.ca
+__updated__ = "2023-09-12"
+-------------------------------------------------------
+"""
+# Imports
+# Constants
+name = input("Please enter your name: ")
+print("Pleased to meet you ")
+print(name)
---
+++ Labs/Verm1154_l01/t06.py
@@ -0,0 +1,18 @@
+"""
+-------------------------------------------------------
+[program description]
+-------------------------------------------------------
+Author:  Himanya Verma
+ID:      169051154
+Email:   verm1154@mylaurier.ca
+__updated__ = "2023-09-12"
+-------------------------------------------------------
+"""
+# Imports
+# Constants
+
+# Calculate total pay
+salary = 2500.0
+bonus = 1200.0
+pay = salary + bonus
+print('Your pay is', pay)
