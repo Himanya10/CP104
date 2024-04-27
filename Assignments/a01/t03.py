8911ffdcc675eb1e08df0a182d1e929ebdbce3d3
Himanya10 <145639852+Himanya10@users.noreply.github.com>
2024-04-26 20:00:51 -0400

    Add files via upload

---
+++ Assignments/a01/t03.py
@@ -0,0 +1,15 @@
+"""
+-------------------------------------------------------
+Converting Miles to KM
+-------------------------------------------------------
+Author:  Himanya Verma
+ID:      169051154
+Email:   verm1154@mylaurier.ca
+__updated__ = "2023-09-20"
+-------------------------------------------------------
+"""
+# Imports
+# Constants
+Miles = float(input("Length in miles: "))
+KM = Miles * 1.61
+print(f"Length in km: {KM}")
---
+++ Assignments/a01/t04.py
@@ -0,0 +1,17 @@
+"""
+-------------------------------------------------------
+Calculating how much an individual would have to pay for dosas
+-------------------------------------------------------
+Author:  Himanya Verma
+ID:      169051154
+Email:   verm1154@mylaurier.ca
+__updated__ = "2023-09-20"
+-------------------------------------------------------
+"""
+# Imports
+# Constants
+
+Cost = float(input("Cost of 1 dosa: $ "))
+Dosas = int(input("Number of dosa: "))
+Total = Cost * Dosas
+print(f"Total cost of {Dosas} dosas: ${Total} ")
